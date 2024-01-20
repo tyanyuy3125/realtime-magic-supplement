ba0927d3e5e4bb56ef57984d26cb69c9d9f778d4
Tianyu Huang <tianyu@illumiart.net>
2024-01-20 17:59:14 +0800

    ggx, lambert & volumetric

--- brdf/lambertian.m
+++ brdf/lambertian.m
@@ -9,7 +9,8 @@ n_phi = 64;
 [tehta,phi]=meshgrid(0:pi/2./(n_tehta-1):pi/2,...
 0:2*pi./(n_phi-1):2*pi) ;
 
-Bas = 0.5;
+theta_i = deg2rad(45);
+phi_i = deg2rad(0);
 radio = 1/(pi);
 
 X=radio.*sin(tehta).*cos(phi);
@@ -17,5 +18,9 @@ Y=radio.*sin(tehta).*sin(phi);
 Z=radio.*cos(tehta);
 
 mesh(X,Y,Z);
+hold on;
+quiver3([0 0],[0 0],[0 0],[sin(theta_i)*cos(phi_i)/2 0],[sin(theta_i)*sin(phi_i)/2 0],[cos(theta_i)/2 1/2],'Color','b');
+text(sin(theta_i)*cos(phi_i)/2,sin(theta_i)*sin(phi_i)/2,cos(theta_i)/2,'Incident','FontSize',15,'Color','b');
+text(0,0,1/2,'Normal','FontSize',15,'Color','b');
 title('Lambertian BRDF');
 axis image;
--- brdf/orennayar.m
+++ brdf/orennayar.m
@@ -7,26 +7,26 @@ close all hidden
 n_theta = 128;
 n_phi = 128;
 [theta,phi]=meshgrid(0:pi/2./(n_theta-1):pi/2,...
-0:2*pi./(n_phi-1):2*pi);
+0:2*pi./(n_phi-1):2*pi) ;
 
-X=sin(theta).*cos(phi);
-Y=sin(theta).*sin(phi);
-Z=cos(theta);
-size(X)
-omega_o = [X;Y;Z];
-size(omega_o)
-
-Bas = 0.5;
-k_a = 0.1;
-k_d = 0.2;
-k_s = 1.0;
-radio = k_a + k_d * max(0, cos(theta_i)) %+ k_s * max(0, );
+sigma = 0.5;
+theta_i = deg2rad(60);
+phi_i = deg2rad(0);
+A = 1-(sigma^2)/(2*(sigma^2+0.33));
+B = (0.45*sigma^2)/(sigma^2+0.09);
+alpha = max(theta_i, theta);
+beta = min(theta_i, theta);
+radio = (1/pi).*(A+B.*max(0, cos(phi_i-phi)).*sin(alpha).*tan(beta));
 
 X=radio.*sin(theta).*cos(phi);
 Y=radio.*sin(theta).*sin(phi);
 Z=radio.*cos(theta);
 
 mesh(X,Y,Z);
+hold on;
+quiver3([0 0],[0 0],[0 0],[sin(theta_i)*cos(phi_i)/2 0],[sin(theta_i)*sin(phi_i)/2 0],[cos(theta_i)/2 1/2],'Color','b');
+text(sin(theta_i)*cos(phi_i)/2,sin(theta_i)*sin(phi_i)/2,cos(theta_i)/2,'Incident','FontSize',15,'Color','b');
+text(0,0,1/2,'Normal','FontSize',15,'Color','b');
 title('Oren-Nayar BRDF');
 axis image;
 
